b04b62b02e89e75aac3ec8ef155a2945e0788d66
imxande <alerodriguezlarosa@gmail.com>
2022-05-01 01:59:46 -0400

    setting up initial commit for customer validation method

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,12 +1,16 @@
 // import express and create a router
 const router = require("express").Router();
 
+// import customer validation
+const customerValidation = require("../customers/customerValidation");
+
 // register endpoint and handler
 router.post("/register", (req, res) => {
   // store request body
   const customer = req.body;
 
   // validate that the body of request contains the related info needed to create a new customer
+  const validated = customerValidation(customer);
   // if info is good to go let us add the new customer info to our data base
   // if not we will need to send an error message base on what went wrong and how to fix it
 
