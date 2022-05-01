4811b4c9165166368db3a2edc54857f933623ff3
imxande <alerodriguezlarosa@gmail.com>
2022-05-01 01:44:34 -0400

    stored request data and send it back, broke down registration process

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,9 +3,15 @@ const router = require("express").Router();
 
 // register endpoint and handler
 router.post("/register", (req, res) => {
-  res.json({
-    message: "Response from auth router! ",
-  });
+  // store request body
+  const customer = req.body;
+
+  // validate that the body of request contains the related info needed to create a new customer
+  // if info is good to go let us add the new customer info to our data base
+  // if not we will need to send an error message base on what went wrong and how to fix it
+
+  // send response back
+  res.send(customer);
 });
 
 module.exports = router;
