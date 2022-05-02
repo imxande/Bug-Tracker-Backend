fd98ba8957817e0cf35702a3860d1ca78a87dccc
imxande <alerodriguezlarosa@gmail.com>
2022-05-01 21:11:47 -0400

    completed customer validation

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,25 +1,29 @@
 // import express and create a router
-const router = require("express").Router();
+const router = require( "express" ).Router();
 
 // import customer validation
-const customerValidation = require("../customers/customerValidation");
+const customerValidation = require( "../customers/customerValidation" );
 
 // register endpoint and handler
-router.post("/register", (req, res) => {
-  // store request body
-  const customer = req.body;
+router.post( "/register", ( req, res ) =>
+{
+    // store request body
+    const customer = req.body;
 
-  // validate that the body of request contains the related info needed to create a new customer
-  const validated = customerValidation(customer);
-  // if customer data from request body is good then create customer in data in data base and send back the newly created customer
-  if (validated.isSuccessful) {
-    // if info is good to go let us add the new customer info to our data base
-    res.send(customer);
-  }
-  // if not we will need to send an error message base on what went wrong and how to fix it
-  else {
-    res.send(validated.error);
-  }
-});
+    // validate that the body of request contains the related info needed to create a new customer
+    const validated = customerValidation( customer );
+
+    // if customer data from request body is good then create customer in data in data base and send back the newly created customer
+    if ( validated.isSuccessful )
+    {
+        // if info is good to go let us add the new customer info to our data base
+        res.send( customer );
+    }
+    // if not we will need to send an error message base on what went wrong and how to fix it
+    else
+    {
+        res.send( validated.error );
+    }
+} );
 
 module.exports = router;
