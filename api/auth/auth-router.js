e2749ececd0ce4b91be4e7fc20cdd8210cb3bf2d
imxande <alerodriguezlarosa@gmail.com>
2022-05-03 11:55:33 -0400

    used bcrypt npm module to hash customer password

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,5 @@
 // import bcrypt
-// const bcrypt = require( "bcrypt" );
+const bcrypt = require( "bcrypt" );
 
 // import express and create a router
 const router = require( "express" ).Router();
@@ -14,7 +14,21 @@ const { validateFirstName, validateLastName, validateEmail, validatePassword } =
 //**************************** REGISTER endpoint and handler ****************************
 router.post( "/register", validateFirstName, validateLastName, validateEmail, validatePassword, ( req, res ) =>
 {
-    res.send( "Register is good to go" );
+    // grab customer info from request body
+    const { fistName, lastName, email, password } = req.body;
+
+    // salt rounds for our hash
+    const saltRounds = 10;
+
+    // hash password
+    bcrypt.hash( password, saltRounds, ( err, hash ) =>
+    {
+        // store hash in data base
+        res.send( hash );
+    } );
+
+
+
 } );
 
 module.exports = router;
