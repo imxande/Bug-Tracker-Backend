b06dad4fb9ad89b8ffc1dcc0f08f54f291c3acb2
imxande <alerodriguezlarosa@gmail.com>
2022-05-03 19:04:22 -0400

    adde customers-model

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -7,28 +7,46 @@ const router = require( "express" ).Router();
 // import customer validation
 const { validateFirstName, validateLastName, validateEmail, validatePassword } = require( "../auth/auth-middleware" );
 
+// import customer model 
+const Customers = require( "../customers/customers-model" );
+
 /* 
 *********************************END POINTS 👇**********************************************
 */
 
 //**************************** REGISTER endpoint and handler ****************************
-router.post( "/register", validateFirstName, validateLastName, validateEmail, validatePassword, ( req, res ) =>
+router.post( "/register", validateFirstName, validateLastName, validateEmail, validatePassword, async ( req, res, next ) =>
 {
-    // grab customer info from request body
-    const { fistName, lastName, email, password } = req.body;
-
-    // salt rounds for our hash
-    const saltRounds = 10;
-
-    // hash password
-    bcrypt.hash( password, saltRounds, ( err, hash ) =>
+    try
     {
-        // store hash in data base
-        res.send( hash );
-    } );
+        // grab customer info from request body
+        const customer = req.body;
+
+
+        // salt rounds for our hash
+        const saltRounds = 10;
+
+        // hash password
+        const hash = bcrypt.hashSync( customer.password, saltRounds );
+
+        // assign the hash to customer password
+        customer.password = hash;
+
+        // Send customer info to the database and store the return value we get from adding customer into the data base
+        //     const response = await Customers.add(customer).then(saved => {
+
+        //     })
+
+        //     } 
+
+        //     catch(error) {
+        //         res.status(500).json(error.message)
+        //     }
 
 
 
-} );
 
-module.exports = router;
+
+        // } );
+
+        module.exports = router;
