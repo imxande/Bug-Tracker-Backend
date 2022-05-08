acbb2a19597c2c3aacc77c9799776dfd7d84e968
imxande <alerodriguezlarosa@gmail.com>
2022-05-08 15:48:12 -0400

    updated migrations

--- data/migrations/20220508163845_customers-schema-update.js
+++ data/migrations/20220508163845_customers-schema-update.js
@@ -4,23 +4,57 @@
  */
 exports.up = function ( knex )
 {
-    // create schema
-    return knex.schema.createTable( "customers", ( table ) =>
-    {
-        // create customer id column
-        table.increments( "customer_id" );
-        // customer first name
-        table.string( "firstName", 64 ).notNullable();
-        // customer last name
-        table.string( "lastName", 64 ).notNullable();
-        // email
-        table.string( "email", 64 ).notNullable().unique();
-        // here we will store the hash
-        table.varchar( "password", 255 ).notNullable();
-        // customers will have a regular user role
-        table.string( "role", 12 ).notNullable();
+    // return schema
+    return knex.schema
+        // create customers table
+        .createTable( "customers", ( table ) =>
+        {
+            // create customer id column
+            table.increments( "customer_id" );
+            // customer first name
+            table.string( "firstName", 64 ).notNullable();
+            // customer last name
+            table.string( "lastName", 64 ).notNullable();
+            // email
+            table.string( "email", 64 ).notNullable().unique();
+            // here we will store the hash
+            table.varchar( "password", 255 ).notNullable();
+            // customers will have a regular user role
+            table.string( "role", 12 ).notNullable();
+        } )
+        // create employees table
+        .createTable( "employees", ( table ) =>
+        {
+            // create employees id
+            table.increments( "employee_id" );
+            // employees first name
+            table.string( "firstName", 64 ).notNullable();
+            // employees last name
+            table.string( "lastName", 64 ).notNullable();
+            // employees email
+            table.string( "email", 64 ).notNullable().unique();
+            // employees password
+            table.varchar( "password", 255 ).notNullable();
+            // employees role
+            table.string( "role", 12 ).notNullable();
+        } )
+        // create tickets table
+        .createTable( "tickets", ( table ) =>
+        {
+            // create a ticket id
+            table.increments( "ticket_id" );
+            // reference to customer that created the ticket
+            table.integer( "customer_id" ).unsigned().notNullable().references( "customer_id" ).inTable( "customers" ).onDelete( "CASCADE" ).onUpdate( "CASCADE" );
+            // reference to employee that has been assigned to solve the ticket
+            table.integer( "employee_id" ).unsigned().notNullable().references( "employee_id" ).inTable( "employees" ).onDelete( "CASCADE" ).onUpdate( "CASCADE" );
+            // create ticket subject 
+            table.string( "subject", 60 ).notNullable();
+            // date the ticket was created
+            table.string( "date", 20 ).notNullable();
+            // ticket status
+            table.string( "status", 11 ).notNullable();
 
-    } );
+        } );
 
 };
 
