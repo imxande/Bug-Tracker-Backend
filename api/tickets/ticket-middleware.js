9ac6c148e42fdd460cb621337424295313018c2c
imxande <alerodriguezlarosa@gmail.com>
2022-05-20 01:36:54 -0400

    formatted ticket middleware

--- api/tickets/ticket-middleware.js
+++ api/tickets/ticket-middleware.js
@@ -1,24 +1,22 @@
 // import ticket model
-const { getTicketById } = require( "../tickets/tickets-model" );
+const { getTicketById } = require("../tickets/tickets-model");
 
 // check if a ticket exist
-const ticketPresence = async ( req, res, next ) =>
-{
-    // grab ticket id from params
-    const { id } = req.params;
+const ticketPresence = async (req, res, next) => {
+	// grab ticket id from params
+	const { id } = req.params;
 
-    // get ticket by id
-    const ticket = await getTicketById( id );
+	// get ticket by id
+	const ticket = await getTicketById(id);
 
-    // check if ticket exist
-    if ( !ticket )
-    {
-        // send message with Not Found status code
-        res.status( 404 ).json( `Ticket with id of ${ id } does not exist!` );
-    }
+	// check if ticket exist
+	if (!ticket) {
+		// send message with Not Found status code
+		res.status(404).json(`Ticket with id of ${id} does not exist!`);
+	}
 
-    // otherwise wrap everything up and call next middleware
-    else next();
+	// otherwise wrap everything up and call next middleware
+	else next();
 };
 
 module.exports = ticketPresence;
