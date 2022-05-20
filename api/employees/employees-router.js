1dcdfaa0bf9c6483c2541c1a428ba6002051435d
imxande <alerodriguezlarosa@gmail.com>
2022-05-20 01:23:13 -0400

    formatted employees router

--- api/employees/employees-router.js
+++ api/employees/employees-router.js
@@ -1,32 +1,32 @@
-// import express and create a router
-const router = require( "express" ).Router();
+// imports
+const router = require("express").Router();
+const { restricted, adminAccess } = require("../auth/auth-middleware");
 const {
-    createEmployee,
-    getAllEmployees,
-    getEmployeeById,
-    updateEmployee,
-    deleteEmployee
-} = require( "../employees/employees-model" );
-const { restricted, adminAccess } = require( "../auth/auth-middleware" );
+	createEmployee,
+	getAllEmployees,
+	getEmployeeById,
+	updateEmployee,
+	deleteEmployee,
+} = require("../employees/employees-model");
 
 /**
  * @api {get} /api/employees List all Employees
  * @apiVersion 1.0.0
  * @apiName GetEmployees
  * @apiGroup Employees
- * 
+ *
  * @apiHeader {String} jsonwebtoken Employees unique access token
- * @apiHeaderExample {json} Header-Example:              
+ * @apiHeaderExample {json} Header-Example:
  * { "Authorization": "aklsdfuhajwejn;aglkasgjasoidgasf##$$sjfaisdfoi"}
- * 
+ *
  * @apiSuccess {Object[]} Employees List of all Employees
- * @apiSuccess {Number} employee_id Employee ID 
+ * @apiSuccess {Number} employee_id Employee ID
  * @apiSuccess {String} firstname Employee Firstname
- * @apiSuccess {String} lastname Employee Lastname 
- * @apiSuccess {String} email Employee email 
- * @apiSuccess {String} password Employee Password 
- * @apiSuccess {String} role Employee Role 
- * 
+ * @apiSuccess {String} lastname Employee Lastname
+ * @apiSuccess {String} email Employee email
+ * @apiSuccess {String} password Employee Password
+ * @apiSuccess {String} role Employee Role
+ *
  * @apiSuccessExample {Object[]}  Success-Response:
  * HTTP/1.1 200 OK
  *      [
@@ -47,52 +47,47 @@ const { restricted, adminAccess } = require( "../auth/auth-middleware" );
  *              "role": "user"
  *          },
  *     ]
- * 
+ *
  * @apiError (Employees-Error) {String} Forbidden Not authorized
  * @apiErrorExample {String} Error-Response:
  *      HTTP 1.1 403 Forbidden
  *      "Permission denied, not token found"
- * 
+ *
  * @apiError (Employees-Error) {json} Unauthorized Not authorized
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 401 Unauthorized
  *      {
  *           message: "JWT malformed"
  *      }
- * 
+ *
  * @apiError (Employees-Error) {String} Forbidden Not administrator
  * @apiErrorExample {String} Error-Response:
  *      HTTP 1.1 403 Forbidden
  *      "Permission denied, not an admin user"
  */
-router.get( "/", restricted, adminAccess, async ( req, res, next ) =>
-{
-    try
-    {
-        // grab all employees from data base
-        const employees = await getAllEmployees();
+router.get("/", restricted, adminAccess, async (req, res, next) => {
+	try {
+		// grab all employees from data base
+		const employees = await getAllEmployees();
 
-        // return all the employees
-        res.status( 200 ).json( employees );
-    }
-
-    catch ( error )
-    {
-        // send error to client
-        next( { error } );
-    }
-} );
+		// return all the employees
+		res.status(200).json(employees);
+	} catch (error) {
+		// send error to client
+		next({ error });
+	}
+});
 
 /**
  * @api {get} /api/employees/:id Employees unique id
  * @apiName GetEmployee
  * @apiVersion 1.0.0
  * @apiGroup Employee
- * 
+ *
  * @apiHeader {String} jsonwebtoken Admin unique access token
- * @apiHeaderExample {json} Header-Example: 
+ * @apiHeaderExample {json} Header-Example:
  * { "Authorization": "aklsdfuhajwejn;aglkasgjasoidgasf##$$sjfaisdfoi"}
- * 
+ *
  * @apiSuccess {json} Employee Information
  * @apiSuccess {Number} employee_id ID
  * @apiSuccess {String} firstName Employee Firstname
@@ -100,7 +95,7 @@ router.get( "/", restricted, adminAccess, async ( req, res, next ) =>
  * @apiSuccess {String} email Employee Email
  * @apiSuccess {String} password Employee Password
  * @apiSuccess {String} role Employee Role
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 Ok
  *      {
@@ -111,56 +106,51 @@ router.get( "/", restricted, adminAccess, async ( req, res, next ) =>
  *          "password": "$2b$10$TA.fITJQ4gfT4w6HQizbrORraKBn9lWO5FInKUpr712bFko4ZY5/i",
  *          "role": "user"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {json} Unauthorized Not authorized
  * @apiErrorExample {json} 401 Unauthorized
  *      {
  *          "message": "JWT malformed"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {String} Forbidden Not authorized
  * @apiErrorExample {String} Error-Response:
  *      HTTP/1.1 403 Forbidden
  *      "Permission Denied"
- * 
+ *
  * @apiError (Employee-Error) {String} Forbidden Not administrator
  * @apiErrorExample {String} Error-Response:
  *      HTTP/1.1 403 Forbidden
  *      "Permission denied, not an admin user"
  */
-router.get( "/:id", restricted, adminAccess, async ( req, res, next ) =>
-{
-    try
-    {
-        // grab id from parameter
-        const { id } = req.params;
+router.get("/:id", restricted, adminAccess, async (req, res, next) => {
+	try {
+		// grab id from parameter
+		const { id } = req.params;
 
-        // find employee by id
-        const employee = await getEmployeeById( id );
+		// find employee by id
+		const employee = await getEmployeeById(id);
 
-        // send status with the employee
-        res.status( 200 ).json( employee );
-    }
-
-    catch ( error )
-    {
-        // send error to client
-        next( { error } );
-    }
-} );
+		// send status with the employee
+		res.status(200).json(employee);
+	} catch (error) {
+		// send error to client
+		next({ error });
+	}
+});
 
 /**
  * @api {post} /api/employees/:id Create new employee
  * @apiVersion 1.0.0
  * @apiName CreateEmployee
  * @apiGroup Employee
- * 
+ *
  * @apiParam {String} firstname Employee Firstname
  * @apiParam {String} lastname Employee Lastname
  * @apiParam {String} email Employee Email
  * @apiParam {String} password Employee Password
  * @apiParam {String} role Employee User Role
- * 
+ *
  * @apiParamExample {json} Input Request Example:
  *    {
  *     "firstName": "Firstname",
@@ -169,11 +159,11 @@ router.get( "/:id", restricted, adminAccess, async ( req, res, next ) =>
  *     "password": "password",
  *     "role": "admin"
  *    }
- * 
- * 
+ *
+ *
  * @apiSuccessExample {json} Success Response Output:
  *  HTTP/1.1 201 Created
- * 
+ *
  *    {
  *      "employee_id": 20
  *      "firstName": "Firstname",
@@ -182,47 +172,47 @@ router.get( "/:id", restricted, adminAccess, async ( req, res, next ) =>
  *      "password": "$$2b$10$hVDly.4Mlfpu2tSVjZtnbu7nUsxWLnDT8Qr8JgFxhH5WGPSj6LVLG",
  *      "role": "admin"
  *    }
- * 
+ *
  * @apiError (Employee-Error) {String}  BadRequest The entered email already has an associated account.
  * @apiErrorExample {String} Error-Response:
  *      HTTP 1.1 400 Bad Request
  *      "Email provided is already associated with an account"
- * 
+ *
  * @apiError (Employee-Error) {json} BadRequest Firstname param empty.
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 400 Bad Request
  *      {
  *          "errorMessage": "Not content, firstname is empty please provide first name"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {json} BadRequest Lastname param empty.
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 400 Bad Request
  *      {
  *         "errorMessage": "Error, lastname not added, please make sure to add last name"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {json} BadRequest Email param empty.
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 400 Bad Request
  *      {
  *         "errorMessage": "Error, email is empty please send email address"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {json} BadRequest Password param empty.
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 400 Bad Request
  *      {
  *         "errorMessage": "Error, password not provided, please create a password"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {json} BadRequest Password param empty.
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 400 Bad Request
  *      {
  *         "errorMessage": "Error, password not provided, please create a password"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {json} Bad Request Role param empty.
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 400 Bad Request
@@ -230,49 +220,44 @@ router.get( "/:id", restricted, adminAccess, async ( req, res, next ) =>
  *         "errorMessage": "Error, role not provided, please make sure to include a role"
  *      }
  */
-router.post( "/", restricted, adminAccess, async ( req, res, next ) =>
-{
-    try
-    {
-        // grab employee's info from request body
-        const employee = req.body;
+router.post("/", restricted, adminAccess, async (req, res, next) => {
+	try {
+		// grab employee's info from request body
+		const employee = req.body;
 
-        // add employee to data base using create employee method from employee's model
-        const newEmployee = await createEmployee( employee );
+		// add employee to data base using create employee method from employee's model
+		const newEmployee = await createEmployee(employee);
 
-        // send status and the new employee in the response
-        res.status( 201 ).json( newEmployee );
-    }
-
-    catch ( error )
-    {
-        // send error to client
-        next( { error } );
-    }
-} );
+		// send status and the new employee in the response
+		res.status(201).json(newEmployee);
+	} catch (error) {
+		// send error to client
+		next({ error });
+	}
+});
 
 /**
  * @api {put} /api/employee/:id Edit Employee
  * @apiName EditEmployee
  * @apiGroup Employee
  * @apiVersion 1.0.0
- * 
+ *
  * @apiHeader {String} jsonwebtoken Employee unique access token
  * @apiHeaderExample {json} Header-Example:
  * * { "Authorization": "aklsdfuhajwejn;aglkasgjasoidgasf##$$sjfaisdfoi"}
- * 
+ *
  * @apiParam {json} payload Payload should be an object with the changes
  * @apiDescription Edit customer description
  * To edit an employee make sure to send in the header the jsonwebtoken
  * The body of the request should include at least a change to make to the employee
- * 
+ *
  * @apiParamExample {json} Input Request body:
  *      {
  *          firstName: "Change",
- *          lastName: "Change",         
+ *          lastName: "Change",
  *      }
- * 
- * @apiSuccess {json} employee Updated Employee Object 
+ *
+ * @apiSuccess {json} employee Updated Employee Object
  * @apiSuccess {json} message Message
  * @apiSuccessExample {json} Success-Response
  * HTTP/1.1 200 Ok
@@ -287,68 +272,63 @@ router.post( "/", restricted, adminAccess, async ( req, res, next ) =>
  *         },
  *         "message": "Employee has been updated!"
  *      }
- * 
- * 
+ *
+ *
  * @apiBody {json} jsonwebtoken JWT Mandatory json web token
  * @apiBody {json} payload Mandatory changes to make at least 1 change
- * 
+ *
  * @apiError (Customer-Error) {json} Unauthorized Not authorized
  * @apiErrorExample {json} 401 Unauthorized
  *      {
  *          "message": "JWT malformed"
  *      }
- * 
+ *
  * @apiError (Customer-Error) {String} Forbidden Not authorized
  * @apiErrorExample {String} Error-Response:
  *      HTTP/1.1 403 Forbidden
  *      "Permission Denied"
- * 
+ *
  * @apiError (Customer-Error) {String} Forbidden Not administrator
  * @apiErrorExample {String} Error-Response:
  *      HTTP/1.1 403 Forbidden
  *      "Permission denied, not an admin user"
- * 
+ *
  * */
-router.put( "/:id", restricted, adminAccess, async ( req, res, next ) =>
-{
-    try
-    {
-        // grab id from parameters
-        const { id } = req.params;
+router.put("/:id", restricted, adminAccess, async (req, res, next) => {
+	try {
+		// grab id from parameters
+		const { id } = req.params;
 
-        // grab changes from request body
-        const changes = req.body;
+		// grab changes from request body
+		const changes = req.body;
 
-        // update employee with changes
-        await updateEmployee( id, changes );
+		// update employee with changes
+		await updateEmployee(id, changes);
 
-        //  find employee
-        const employee = await getEmployeeById( id );
+		//  find employee
+		const employee = await getEmployeeById(id);
 
-        // send status and the updated employee
-        res.status( 200 ).json( {
-            ...employee,
-            message: "Employee has been updated"
-        } );
-    }
-
-    catch ( error )
-    {
-        // send error to client
-        next( { error } );
-    }
-} );
+		// send status and the updated employee
+		res.status(200).json({
+			...employee,
+			message: "Employee has been updated",
+		});
+	} catch (error) {
+		// send error to client
+		next({ error });
+	}
+});
 
 /**
  * @api {delete} /api/employees/:id Delete an employee
  * @apiName DeleteEmployee
  * @apiGroup Employee
  * @apiVersion 1.0.0
- * 
+ *
  * @apiHeader {String} jsonwebtoken Admin unique access token
- * @apiHeaderExample {json} Header-Example: 
+ * @apiHeaderExample {json} Header-Example:
  * { "Authorization": "aklsdfuhajwejn;aglkasgjasoidgasf##$$sjfaisdfoi"}
- * 
+ *
  * @apiSuccessExample {json} Success-Response:
  * HTTP/1.1 200 Ok
  *      {
@@ -359,44 +339,39 @@ router.put( "/:id", restricted, adminAccess, async ( req, res, next ) =>
  *          "password": "$2b$10$NJxZd38RiKpbyjYmNz6FJueqTN/9UQ7/r7XfnLwDnYFwbKp3EfP6.",
  *          "role": "admin"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {String} Forbidden Not authorized
  * @apiErrorExample {String} Error-Response:
  *      HTTP 1.1 403 Forbidden
  *      "Permission denied, not token found"
- * 
+ *
  * @apiError (Employee-Error) {json} Unauthorized Not authorized
  * @apiErrorExample {json} Error-Response:
  *      HTTP 1.1 401 Unauthorized
  *      {
  *           message: "JWT malformed"
  *      }
- * 
+ *
  * @apiError (Employee-Error) {String} Forbidden Not administrator
  * @apiErrorExample {String} Error-Response:
  *      HTTP 1.1 403 Forbidden
  *      "Permission denied, not an admin user"
- * 
+ *
  */
-router.delete( "/:id", restricted, adminAccess, async ( req, res, next ) =>
-{
-    try
-    {
-        // grab id from params
-        const { id } = req.params;
+router.delete("/:id", restricted, adminAccess, async (req, res, next) => {
+	try {
+		// grab id from params
+		const { id } = req.params;
 
-        // grab deleted employee
-        const employee = await deleteEmployee( id );
+		// grab deleted employee
+		const employee = await deleteEmployee(id);
 
-        // return status and the deleted employee
-        res.status( 200 ).json( employee );
-    }
-
-    catch ( error )
-    {
-        // send error to client
-        next( { error } );
-    }
-} );
+		// return status and the deleted employee
+		res.status(200).json(employee);
+	} catch (error) {
+		// send error to client
+		next({ error });
+	}
+});
 
 module.exports = router;
