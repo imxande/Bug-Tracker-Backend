75d8cdf965093c78958788b2d3efaa6bcf9e2189
imxande <alerodriguezlarosa@gmail.com>
2022-05-22 19:15:42 -0400

    added better description of the test

--- api/auth/auth.middleware.spec.js
+++ api/auth/auth.middleware.spec.js
@@ -1,6 +1,5 @@
-// Checking if we are testing in the TESTING environment
 describe("environment", () => {
-	test("is the testing environment", () => {
+	test("is the correct environment to run our test", () => {
 		expect(process.env.DB_ENV).toBe("testing");
 	});
 });
