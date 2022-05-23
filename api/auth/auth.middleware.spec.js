fb37b53267283fb19569aefe140d9c723e6ef7a7
imxande <alerodriguezlarosa@gmail.com>
2022-05-23 00:07:04 -0400

    validate first name initial commit

--- api/auth/auth.middleware.spec.js
+++ api/auth/auth.middleware.spec.js
@@ -1,5 +1,11 @@
+const { validateFirstName } = require("./auth-middleware");
+
 describe("environment", () => {
 	test("is the correct environment to run our test", () => {
 		expect(process.env.DB_ENV).toBe("testing");
 	});
+});
+
+describe("validateFirstName", () => {
+	test("checks if there is a firstname in the request body", () => {});
 });
