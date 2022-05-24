41a317ae1ae2696347c9f94b27d43b5932d0fb88
imxande <alerodriguezlarosa@gmail.com>
2022-05-24 17:26:26 -0400

    added beforeall beforeeach and afterall globals

--- api/auth/auth-router.test.js
+++ api/auth/auth-router.test.js
@@ -1,5 +1,28 @@
+// import our database configuration
+const db = require("../../data/config/dbConfig");
+
 describe("environment", () => {
 	test("is the correct environment to run our test", () => {
 		expect(process.env.DB_ENV).toBe("testing");
 	});
 });
+
+// run migrations before each test
+beforeAll(async () => {
+	// roll back first
+	await db.migrate.rollback();
+	// run latest migrations
+	await db.migrate.latest();
+});
+
+// lets run all our seeds before each test
+beforeEach(async () => {
+	await db.seed.run();
+});
+
+// lest disconnect from the data base after all the test are done.
+afterAll(async () => {
+	await db.destroy();
+});
+
+describe("Registration", () => {});
