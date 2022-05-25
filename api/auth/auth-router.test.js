6940ffed8fa741bd2bb63165690716324de5608b
imxande <alerodriguezlarosa@gmail.com>
2022-05-25 17:42:41 -0400

    5 test passed none failure

--- api/auth/auth-router.test.js
+++ api/auth/auth-router.test.js
@@ -3,8 +3,20 @@ const db = require("../../data/config/dbConfig");
 const app = require("../app");
 const request = require("supertest");
 
-// supertest request
-const postRequestRegister = request(app).post("/api/auth/register");
+/**
+ * Our registration end point has a bunch of validators middleware that will validate the payload object
+ * I have added this simple validatePayload function here in order to use supertest to send a malformed payload object
+ * to trigger our validator middleware. Our payload will send missing information. For example when we don't add an email
+ * the validateEmail should be triggered as supertest gets hit with the request with the missing property. Another example could be
+ * that our payload is missing the role or the new user. Well our role validator middleware should be triggered, and so on.
+ *
+ * Please check our auth middleware for a full list of the validator we have created.
+ *
+ */
+const validatePayload = (payload) => {
+	const response = request(app).post("/api/auth/register").send(payload);
+	return response;
+};
 
 // run migrations before each test
 beforeAll(async () => {
@@ -32,37 +44,37 @@ describe("environment", () => {
 
 describe("[POST] /api/auth/register ", () => {
 	it("responds 400 if no email in payload", async () => {
-		let res = await postRequestRegister.send({
+		const response = await validatePayload({
 			customer_id: 5,
 			firstName: "Sy",
 			lastName: "Bnd",
 			password: "pass",
 			role: "user",
 		});
-		expect(res.status).toBe(400);
+		expect(response.status).toBe(400);
 	});
 	it("responds 400 if no role in payload", async () => {
-		const res = await postRequestRegister.send({
+		const response = await validatePayload({
 			customer_id: 5,
 			firstName: "Sy",
 			lastName: "Bnd",
 			email: "sy@test.tst",
 			password: "pass",
 		});
-		expect(res.status).toBe(400);
+		expect(response.status).toBe(400);
 	});
 	it("responds 400 if no password in payload", async () => {
-		const res = await postRequestRegister.send({
+		const response = await validatePayload({
 			customer_id: 5,
 			firstName: "Syd",
 			lastName: "Bndd",
 			email: "sy@test.tdst",
 			role: "user",
 		});
-		expect(res.status).toBe(400);
+		expect(response.status).toBe(400);
 	});
 	it("responds error message if no password in payload", async () => {
-		const res = await postRequestRegister.send({
+		const response = await validatePayload({
 			customer_id: 5,
 			firstName: "Sfy",
 			lastName: "Bndg",
@@ -70,12 +82,7 @@ describe("[POST] /api/auth/register ", () => {
 			role: "user",
 		});
 
-		const email = res.email;
-		console.log(email);
-		const error = res.body.errorMessage;
-		console.log(error);
-
-		expect(res.body.errorMessage).toBe(
+		expect(response.body.errorMessage).toBe(
 			"Error, password is empty, please make sure to provide a password in the request"
 		);
 	});
